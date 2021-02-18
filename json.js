f0986dfd02fd02d3a35be3a3b45b9394f87fc25b
Aimarall <aimaral21072004@gmail.com>
2021-02-18 14:04:42 +0600

    added function

--- json.js
+++ json.js
@@ -1,23 +1,34 @@
+// fetch("./data.json")
+//     .then(function (responsive){
+//         return responsive.json();
+//     })
+//     .then(function (data){
+//        document.querySelector('#name').innerText = dats.name.ful;
+//     })
+
+//     .then(function (responsive){
+//         return responsive.json();
+//     })
+//     .then(function (data){
+//        document.querySelector('#age').innerText = dats.age.years;
+//     })
+
+//     .then(function (responsive){
+//         return responsive.json();
+//     })
+//     .then(function (data){
+//        document.querySelector('#hobbies').innerText = dats.hobbies.ful;
+//     })
+
+
 fetch("./data.json")
-    .then(function (responsive){
-        return responsive.json();
-    })
-    .then(function (data){
-       document.querySelector('#name').innerText = dats.name.ful;
-    })
+.then(function (response){
+    return response.json();
 
-    .then(function (responsive){
-        return responsive.json();
-    })
-    .then(function (data){
-       document.querySelector('#age').innerText = dats.age.years;
-    })
+})
 
-    .then(function (responsive){
-        return responsive.json();
-    })
-    .then(function (data){
-       document.querySelector('#hobbies').innerText = dats.hobbies.ful;
-    })
-
+.then(function (data) {
+    document.querySelector('#name').innerText = data.name.full;
+    document.querySelector('#age').innerText = data.age;
+})
     
